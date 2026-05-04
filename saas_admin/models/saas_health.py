11d65cfe144debe80ec96a30c0cfcea38201aa2e
Krishanu-Kabir-Sparsha <krishanukabir@gmail.com>
2026-05-04 13:13:06 +0600

    Modules Activated with Small Fixes

--- saas_admin/models/saas_health.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class SaasSystemHealth(models.Model):
-    _name = 'saas.system.health'
-    _description = 'System Health Monitor'
-    # Will be implemented in Phase 8
-    pass
